40b28182320b037ee2d57d32fdb4a79dd09966bd
jperez <132075076+Juanperezp@users.noreply.github.com>
2024-10-19 22:24:14 -0500

    plantilla pos

---
+++ cajaRegistradora/resources/views/layouts/app.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>@yield('title')</title>
+    <link href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/sb-admin-2.min.css') }}" rel="stylesheet">
+    @stack('styles')
+</head>
+<body id="page-top">
+    <div id="wrapper">
+        @include('partials.sidebar')
+        <div id="content-wrapper" class="d-flex flex-column">
+            <div id="content">
+                @include('partials.navbar')
+                <div class="container-fluid">
+                    @yield('content')
+                </div>
+            </div>
+            @include('partials.footer')
+        </div>
+    </div>
+    <script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('vendor/jquery-easing/jquery.easing.min.js') }}"></script>
+    <script src="{{ asset('js/sb-admin-2.min.js') }}"></script>
+    @stack('scripts')
+</body>
+</html>
---
+++ cajaRegistradora/resources/views/layouts/navbar.blade.php
@@ -0,0 +1,11 @@
+<nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+    <!-- Navbar content -->
+    <ul class="navbar-nav ml-auto">
+        <li class="nav-item dropdown no-arrow">
+            <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown">
+                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Douglas McGee</span>
+                <img class="img-profile rounded-circle" src="{{ asset('img/undraw_profile.svg') }}">
+            </a>
+        </li>
+    </ul>
+</nav>
